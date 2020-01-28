d76be2a5a0f2a92122106da0de7a18f412934d92
Doug Kintop <dkintop@hotmail.com>
2020-01-28 15:32:35 -0500

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -16,7 +16,7 @@ class GifListContainer extends Component {
     );
   }
 
-  fetchGIFs = (query = "dolphins") => {
+  fetchGIFs = (query = "smileys") => {
     fetch(
       `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`
     )
